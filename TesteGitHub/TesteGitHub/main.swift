e79c66d1f4ab7329c1549f77fceec0b2c14e3d45
Fábio Oliveira Alegre <fabioalegre12@gmail.com>
2023-08-17 21:46:52 -0300

    Adicionei 3 prints escritos Olá

---
+++ TesteGitHub/TesteGitHub/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  TesteGitHub
+//
+//  Created by Fábio Oliveira Alegre on 17/08/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+print("Olá")
+print("Olá")
+print("Olá")
